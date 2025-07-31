4b6e65ff117f6f7c630c71cb4a8f06366a029633
Ankit-Mishra2025 <devoankit2025@gmail.com>
2025-07-31 22:27:02 +0530

    Added 3 more questions

--- script.js
+++ script.js
@@ -140,4 +140,128 @@
 
 
      // Question NO -07
+  
 
+    //  -- Map function always return a new array
+
+// let arr=[1,3,7,5]
+// let result=arr.map((square)=>{
+// return square*square
+// })
+
+// console.log(result);
+
+// console.log(arr);
+
+
+// -- ForEach do not return anything ,so always try to console it.
+
+
+// let result=arr.forEach((square)=>{
+// console.log(square*square)
+// })
+  
+// -- find method finds the first element which meets the condition
+
+// let result=arr.find((num)=>{
+// return num%2==0
+// })
+
+// console.log(result);
+
+
+//  FindIndex function find the index of the given logic.
+
+// let result=arr.findIndex((n)=>{
+//     return n%2===0
+// })
+// console.log(result);
+
+ // --  For Some function ,will return true even at least one  element will follow the condition of code
+
+// let result=arr.some((n)=>{
+//     return n%2==0
+// })
+
+// console.log(result);
+
+
+
+// --  For Every function ,will return true when all elements will follow the condition of code
+// let Arr=[1,2,4,6]
+// let result=Arr.every((n)=>{
+// return n%2==0
+// })
+// console.log(result);
+
+
+
+// --  Filter Method
+
+// let arr=[1,2,3,4,5,6,7,8]
+
+// // let pushedNewArr=[]
+
+// let result=arr.filter((n)=>{
+
+// return n%2==0
+
+// })
+
+// console.log(result);
+
+
+// ----Reduce Method
+
+// -- It will return only a single value 
+// let arr=[1,2,4,56,7]
+
+// let result=arr.reduce((acc,value)=>{
+// // let updatedValue=acc*value
+// let updatedValue=acc-value
+// return updatedValue
+// },0)
+
+// console.log(result);
+
+// let sum = 0;
+// let arr = [10,15,20,30];  
+// arr.forEach(function myFunction(element)
+//  {  
+//     sum = sum + element; 
+
+//   });  
+// console.log(sum);
+
+
+// ---- Question No-08
+
+// ----Find the factorial of a number
+
+function factorialCalculate(inputNumber){
+let result=1;
+    for(let i=1; i<=inputNumber; i++){
+        result=result*i
+        
+    }
+      return result
+}
+
+console.log(factorialCalculate(5))
+
+
+// ----Question No=09
+
+// --Find the sum of all elements in an Array
+
+// let arr=[2,3,5,6,8]
+
+// function findSum(inputArray){
+// let sum=0;
+// for(let i=0; i<inputArray.length; i++){
+//     sum+=inputArray[i]
+// }
+// return sum;
+// }
+
+// console.log(findSum(arr))
